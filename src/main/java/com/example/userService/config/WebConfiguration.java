0a48fb981051d6c76ea941f5a2a9b9feb857e946
kim <rlaxogjs1234@naver.com>
2022-07-05 15:51:46 +0900

    webconfiguration cors error

--- src/main/java/com/example/userService/config/WebConfiguration.java
+++ src/main/java/com/example/userService/config/WebConfiguration.java
@@ -1,16 +1,18 @@
-package com.example.userService.config;
+//package com.example.userService.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfiguration implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry){
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedMethods("*")
+//                .allowedHeaders("*");
+//    }
+//}
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
-}
+// gateway에서 한번 체크해주기 때문에 각 서비스에는 필요없음
